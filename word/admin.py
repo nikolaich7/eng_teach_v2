e0f562fc002760768e215506cf0edcbb90135742
nikolaich7 <nikolaich7777777@gmail.com>
2022-03-26 18:58:43 +0300

    admin

--- word/admin.py
+++ word/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Word_en
 
-
-admin.site.register(Word_en)
+@admin.register(Word_en)
+class Word_enAdmin(admin.ModelAdmin):
+    list_display = ['word', 'translation', 'lvl_know']
+    list_display_links = None
+    list_editable = ['word', 'translation', 'lvl_know']
+    list_filter = ['lvl_know']
+    search_fields = ['word', 'translation']
--- word/models.py
+++ word/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Word_en(models.Model):
     word = models.CharField(max_length=25, verbose_name='слово')
-    translation = models.TextField(verbose_name='перевод')
+    translation = models.CharField(max_length=25, verbose_name='перевод')
     lvl_know = models.SmallIntegerField(default=0, verbose_name='уровень знания слова')
 
     def __str__(self):
